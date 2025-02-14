58c581381feca1763665735cb0a9937d6cfd0a7c
Wojciech Rejdych <wrejdych@activecampaign.com>
2025-02-14 13:11:14 +0100

    restore envConfig file

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -1,21 +1,23 @@
 import { httpBatchLink } from '@trpc/client';
 import { createTRPCNext } from '@trpc/next';
 import type { AppRouter } from '../server/trpc';
-
 function getBaseUrl() {
 //   if (typeof window !== 'undefined')
 //     // browser should use relative path
 //     return '';
-  // if (process.env.VERCEL_URL){
-  //   return `https://${process.env.VERCEL_URL}`;
-  // }
+  if (process.env.VERCEL_URL) {
+    // reference for vercel.com
+    return `https://${process.env.VERCEL_URL}`;
+  }
+
 //   if (process.env.RENDER_INTERNAL_HOSTNAME)
 //     // reference for render.com
 //     return `http://${process.env.RENDER_INTERNAL_HOSTNAME}:${process.env.PORT}`;
   // assume localhost
-  return `https://${process.env.VERCEL_URL}`;
+  return `https://ac-desk-reserve.vercel.app`;
 }
-export const trpc = createTRPCNext<AppRouter>({   config(opts) {
+export const trpc = createTRPCNext<AppRouter>({
+  config(opts) {
     return {
       links: [
         httpBatchLink({
