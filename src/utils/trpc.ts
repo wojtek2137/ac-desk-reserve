ec2e2c4fce9e0e8dcf0e5c4ec38bb967b7f862df
Wojciech Rejdych <wrejdych@activecampaign.com>
2025-02-14 11:59:06 +0100

    env process path

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -8,14 +8,14 @@ function getBaseUrl() {
 //   if (typeof window !== 'undefined')
 //     // browser should use relative path
 //     return '';
-  if (process.env.VERCEL_URL){
-    return `https://${process.env.VERCEL_URL}`;
-  }
+  // if (process.env.VERCEL_URL){
+  //   return `https://${process.env.VERCEL_URL}`;
+  // }
 //   if (process.env.RENDER_INTERNAL_HOSTNAME)
 //     // reference for render.com
 //     return `http://${process.env.RENDER_INTERNAL_HOSTNAME}:${process.env.PORT}`;
   // assume localhost
-  return 'https://ac-desk-reserve.vercel.app';
+  return `https://${process.env.VERCEL_URL}`;
 }
 export const trpc = createTRPCNext<AppRouter>({   config(opts) {
     return {
